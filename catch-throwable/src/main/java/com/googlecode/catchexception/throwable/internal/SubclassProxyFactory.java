ff84da598ea9bc83b563b62527e734f2a0b37af2
Mariusz Smykuła <mariuszs@gmail.com>
2014-10-22 21:05:29 +0200

    Removing fest-assert from throwable, fix for new mockit and pom cleaning

--- catch-throwable/src/main/java/com/googlecode/catchexception/throwable/internal/SubclassProxyFactory.java
+++ catch-throwable/src/main/java/com/googlecode/catchexception/throwable/internal/SubclassProxyFactory.java
@@ -19,6 +19,8 @@ import org.mockito.cglib.proxy.MethodInterceptor;
 import org.mockito.exceptions.base.MockitoException;
 import org.mockito.internal.creation.jmock.ClassImposterizer;
 
+import java.lang.reflect.Modifier;
+
 /**
  * This {@link ProxyFactory} uses Mockito's jmock package to create proxies that subclass from the target's class.
  * 
@@ -41,7 +43,7 @@ public class SubclassProxyFactory implements ProxyFactory {
     public <T> T createProxy(Class<?> targetClass, MethodInterceptor interceptor) {
 
         // can we subclass the class of the target?
-        if (!ClassImposterizer.INSTANCE.canImposterise(targetClass)) {
+        if (!canImposterise(targetClass)) {
 
             // delegate
             return fallbackProxyFactory.<T> createProxy(targetClass, interceptor);
@@ -60,5 +62,9 @@ public class SubclassProxyFactory implements ProxyFactory {
         }
 
         return proxy;
+    }
+
+    private boolean canImposterise(Class<?> type) {
+        return !type.isPrimitive() && !Modifier.isFinal(type.getModifiers()) && !type.isAnonymousClass();
     }
 }
---
+++ catch-throwable/src/test/java/com/googlecode/catchexception/throwable/apis/BDDCatchThrowableTest.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (C) 2011 rwoo@gmx.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.catchexception.throwable.apis;
+
+import static com.googlecode.catchexception.throwable.CatchThrowable.caughtThrowable;
+import static com.googlecode.catchexception.throwable.apis.CatchThrowableAssertJ.thenThrown;
+import static com.googlecode.catchexception.throwable.apis.CatchThrowableAssertJ.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Test;
+
+/**
+ * Tests {@link CatchThrowableAssertJ}.
+ * 
+ * @author rwoo
+ * 
+ */
+@SuppressWarnings("javadoc")
+public class BDDCatchThrowableTest {
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testAssertThat() {
+        // given an empty list
+        List myList = new ArrayList();
+
+        // when we try to get first element of the list
+        when(myList).get(1);
+
+        // then we expect an IndexOutOfBoundsException
+        assertThat(caughtThrowable()) //
+                .isInstanceOf(IndexOutOfBoundsException.class) //
+                .hasMessage("Index: 1, Size: 0") //
+                .hasNoCause();
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testThen() {
+        // given an empty list
+        List myList = new ArrayList();
+
+        // when we try to get first element of the list
+        when(myList).get(1);
+
+        // then we expect an IndexOutOfBoundsException
+        then(caughtThrowable()) //
+                .isInstanceOf(IndexOutOfBoundsException.class) //
+                .hasMessage("Index: 1, Size: 0") //
+                .hasMessageStartingWith("Index: 1") //
+                .hasMessageEndingWith("Size: 0") //
+                .hasMessageContaining("Size") //
+                .hasNoCause();
+
+        // test: caughtThrowable() == new RuntimeException()
+        try {
+            then(new RuntimeException()).isInstanceOf(IndexOutOfBoundsException.class);
+
+        } catch (AssertionError e) {
+            assertEquals("\nExpecting:" //
+                    + "\n <java.lang.RuntimeException>" //
+                    + "\nto be an instance of:" //
+                    + "\n <java.lang.IndexOutOfBoundsException>" //
+                    + "\nbut was instance of:" //
+                    + "\n <java.lang.RuntimeException>", e.getMessage());
+        }
+
+        // test: caughtThrowable() has other unexpected message
+        try {
+            then(caughtThrowable()) //
+                    .isInstanceOf(IndexOutOfBoundsException.class) //
+                    .hasMessage("Hi!");
+
+        } catch (AssertionError e) {
+            assertEquals("\nExpecting message:" //
+                    + "\n <\"Hi!\">" //
+                    + "\nbut was:" //
+                    + "\n <\"Index: 1, Size: 0\">", e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testThenThrown() {
+
+        // given a list with nine elements
+        List myList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        // when we try to get the 500th element
+        when(myList).get(500);
+
+        // then we expect an IndexOutOfBoundsException
+        thenThrown(IndexOutOfBoundsException.class);
+
+        // test: caughtThrowable() ==null
+        when(myList).get(0);
+        try {
+            thenThrown(IndexOutOfBoundsException.class);
+
+        } catch (AssertionError e) {
+            assertEquals("Neither a throwable of type java.lang." + "IndexOutOfBoundsException nor another throwable "
+                    + "was thrown", e.getMessage());
+        }
+
+        // test: caughtThrowable() is not IllegalArgumentException
+        when(myList).get(500);
+        try {
+            thenThrown(IllegalArgumentException.class);
+
+        } catch (AssertionError e) {
+            assertEquals("Throwable of type java.lang.IllegalArgumentException"
+                    + " expected but was not thrown. Instead a throwable of"
+                    + " type class java.lang.ArrayIndexOutOfBoundsException" + " with message '500' was thrown.",
+                    e.getMessage());
+        }
+
+    }
+
+}
